a2b485e8afe63af086b867f205ca9e1be82f499d
Alberes Pereira <alberesbass@hotmail.com>
2024-08-20 18:18:46 -0300

    init: 🎉 Primeiro commit do projeto

---
+++ index.js
@@ -0,0 +1,8 @@
+function trocarDeCor() {
+  let elementosRetangulares =
+    document.getElementsByClassName("retanguloSuperior");
+
+  let retanguloDeCima = elementosRetangulares[0];
+
+  retanguloDeCima.style.backgroundColor = "yellow";
+}
